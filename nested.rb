84a02c7a4fec8eecdf8df6d2446ab178f9467773
Denali Balser <denali.balser167@gmail.com>
2019-08-19 12:50:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,19 @@ def adding_matz
         }
      }
 
-	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+	programmer_hash[:yukihiro_matsumoto] = {
+		:known_for => "Ruby",
+		:languages =>
+				["LISP", "C"]
+			}
+
+	#programmer_hash[:yukihiro_matsumoto] = {
+    #known_for: "Ruby",
+    #languages:
+      #["LISP", "C"]
+  #  }
+
+  programmer_hash
 
 
 
