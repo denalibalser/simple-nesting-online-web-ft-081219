de2c6b773674c40df390b44955bf2a428014596d
Denali Balser <denali.balser167@gmail.com>
2019-08-19 12:50:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,12 +91,6 @@ def adding_matz
 				["LISP", "C"]
 			}
 
-	#programmer_hash[:yukihiro_matsumoto] = {
-    #known_for: "Ruby",
-    #languages:
-      #["LISP", "C"]
-  #  }
-
   programmer_hash
 
 
